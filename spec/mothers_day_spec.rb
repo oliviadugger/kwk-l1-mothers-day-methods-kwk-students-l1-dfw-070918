f2b2d9acbf5a36521ef8cfd4186019e3df0c57c5
oliviadugger <oliviacdugger@gmail.com>
2018-07-15 19:49:52 +0000

    automatically backed up by learn

--- spec/mothers_day_spec.rb
+++ spec/mothers_day_spec.rb
@@ -3,7 +3,7 @@ require_relative './spec_helper'
 
 describe "mothers_day.rb" do
 
-  it 'accepts an argument of a name and prints out "Happy Mother\'s Day, <name>!"' do
+  it 'accepts an argument of a name and prints out "Happy Mothers Day, <name>!"' do
     expect(mothers_day("Tracy")).to eq("Happy Mother's Day, Tracy!")
     mothers_day("Tracy")
   end
